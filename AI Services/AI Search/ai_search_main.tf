6119065425531d08f5f545ad70eb1e011a8ca1b4
Monarch Sharma <monarchsharma.m20@gmail.com>
2026-04-21 01:09:13 +0530

    Added the Module code of Ai search service in Feature branch

---
+++ AI Services/AI Search/ai_search_main.tf
@@ -0,0 +1,13 @@
+resource "azurerm_search_service" "ai_search" {
+    name                = var.search_service_name
+    resource_group_name = var.resource_group_name
+    location            = var.location
+    sku = var.ai_search_sku
+
+    dynamic "identity" {
+      for_each = var.enable_managed_identity == true ? [1] : []
+    content {
+      type = var.identity_type
+    }
+    }
+}
---
+++ AI Services/AI Search/ai_search_variable.tf
@@ -0,0 +1,29 @@
+variable "search_service_name" {
+  description = "Name of the AI Search Service (Cognitive Search Service)"
+  type = string
+}
+
+variable "location" {
+  description = "Region of the Search Service"
+  type = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the RG"
+  type = string
+}
+
+variable "ai_search_sku" {
+  description = "SKU tier of AI Search Service: basic, free, standard, standard2, standard3, storage_optimized_l1 and storage_optimized_l2"
+  type = string
+}
+
+variable "enable_managed_identity" {
+  description = "Enable Managed Identity for the instance"
+  type = bool
+}
+
+variable "identity_type" {
+  description = "Type of Managed Identity to create: SystemAssigned or UserAssigned"
+  type = string
+}
