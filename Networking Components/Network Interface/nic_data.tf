1637beca13fab7dcdfb297946f0f6176a9347e90
Monarch Sharma <monarchsharma.m20@gmail.com>
2026-06-25 17:03:20 +0530

    Added Nat Gateway script

---
+++ Networking Components/Network Interface/nic_data.tf
@@ -0,0 +1,5 @@
+# data "azurerm_subnet" "existing_subnet" {
+#   name = var.existing_subnet_name
+#   resource_group_name = var.resource_group_name
+#   virtual_network_name = var.virtual_network_name
+# }
